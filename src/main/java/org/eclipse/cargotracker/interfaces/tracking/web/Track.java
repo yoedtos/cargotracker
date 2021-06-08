54a56c277383b7d6bea41c7c09b929c94ff0c209
Hantsy Bai <hantsy@gmail.com>
2021-06-08 17:33:11 +0800

    chore: clean tracking

--- src/main/java/org/eclipse/cargotracker/interfaces/tracking/web/Track.java
+++ src/main/java/org/eclipse/cargotracker/interfaces/tracking/web/Track.java
@@ -5,6 +5,7 @@ import org.eclipse.cargotracker.domain.model.cargo.CargoRepository;
 import org.eclipse.cargotracker.domain.model.cargo.TrackingId;
 import org.eclipse.cargotracker.domain.model.handling.HandlingEvent;
 import org.eclipse.cargotracker.domain.model.handling.HandlingEventRepository;
+import org.omnifaces.util.Messages;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -69,11 +70,12 @@ public class Track implements Serializable {
                             .getDistinctEventsByCompletionTime();
             this.cargo = new CargoTrackingViewAdapter(cargo, handlingEvents);
         } else {
-            FacesContext context = FacesContext.getCurrentInstance();
-            FacesMessage message =
-                    new FacesMessage("Cargo with tracking ID: " + trackingId + " not found.");
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-            context.addMessage(null, message);
+            Messages.addFlashGlobalError("Cargo with tracking ID: {0} not found.",trackingId);
+//            FacesContext context = FacesContext.getCurrentInstance();
+//            FacesMessage message =
+//                    new FacesMessage("Cargo with tracking ID: " + trackingId + " not found.");
+//            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+//            context.addMessage(null, message);
             this.cargo = null;
         }
     }
